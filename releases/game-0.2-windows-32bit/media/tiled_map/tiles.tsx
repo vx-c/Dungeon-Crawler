888e9f1b66072faa1ea81486ccb7d74bf00b7a4e
vx-c <73669425+vx-c@users.noreply.github.com>
2021-01-07 22:05:47 -0330

    Add files via upload

---
+++ releases/game-0.2-windows-32bit/media/tiled_map/tiles.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tiles" tilewidth="100" tileheight="100" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="100" height="100" source="tile1.jpg"/>
+ </tile>
+ <tile id="1">
+  <image width="100" height="100" source="tile2.jpg"/>
+ </tile>
+ <tile id="2">
+  <image width="100" height="100" source="tile3.jpg"/>
+ </tile>
+ <tile id="3">
+  <image width="100" height="100" source="tile4.jpg"/>
+ </tile>
+</tileset>
